f5e1d1308f554527b39318bdf61be23910534d22
Ghosh <718895@cognizant.com>
2018-09-19 11:57:59 +0530

    added logging via slueth

--- src/main/java/org/cloudfoundry/samples/music/Application.java
+++ src/main/java/org/cloudfoundry/samples/music/Application.java
@@ -7,10 +7,12 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
-@EnableResourceServer
+import java.util.logging.Logger;
+
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
+    private static final Logger LOG = Logger.getLogger(Application.class.getName());
     public static void main(String[] args) {
         new SpringApplicationBuilder(Application.class).
                 initializers(new SpringApplicationContextInitializer())
